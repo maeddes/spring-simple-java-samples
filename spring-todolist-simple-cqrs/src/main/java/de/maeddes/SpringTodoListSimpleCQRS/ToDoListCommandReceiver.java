3c34e847ae653086abc41590e14df3e2c3c55f0c
Matthias Haeussler <Matthias Haeussler>
2017-10-23 09:11:31 +0200

    Added Exception Handling

--- spring-todolist-simple-cqrs/src/main/java/de/maeddes/SpringTodoListSimpleCQRS/ToDoListCommandReceiver.java
+++ spring-todolist-simple-cqrs/src/main/java/de/maeddes/SpringTodoListSimpleCQRS/ToDoListCommandReceiver.java
@@ -13,15 +13,28 @@ public class ToDoListCommandReceiver {
     @RabbitHandler
     public void receive(String in) {
         System.out.println("Received '" + in + "'");
-        if(in.startsWith("done:")){
+        if (in.startsWith("done:")) {
 
             // unsafe hack
             String id = in.split(":")[1];
-            if (id != null) this.toDoItemRepository.delete(new Integer(id));
+            if (id != null) {
+
+                try {
+                    this.toDoItemRepository.delete(new Integer(id));
+                } catch (Exception e) {
+                    System.out.println("Exception: " + e);
+                }
+            }
             return;
 
+        } else {
+
+            try {
+                this.toDoItemRepository.save(new ToDoItem(in));
+            } catch (Exception e) {
+                System.out.println("Exception: " + e);
+            }
         }
-        else this.toDoItemRepository.save(new ToDoItem(in));
     }
 
 }
