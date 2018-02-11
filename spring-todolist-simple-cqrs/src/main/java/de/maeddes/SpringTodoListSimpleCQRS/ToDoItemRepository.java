caec7bb62836857819f6c0cd07d34ee7f52e75eb
Matthias Haeussler <matthias.haeussler@novatec-gmbh.de>
2018-02-11 20:50:30 +0100

    updated

--- spring-todolist-simple-cqrs/src/main/java/de/maeddes/SpringTodoListSimpleCQRS/ToDoItemRepository.java
+++ spring-todolist-simple-cqrs/src/main/java/de/maeddes/SpringTodoListSimpleCQRS/ToDoItemRepository.java
@@ -1,8 +1,8 @@
 package de.maeddes.SpringTodoListSimpleCQRS;
 
-import org.springframework.data.repository.PagingAndSortingRepository;
-
 import java.util.List;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 //@RepositoryRestResource(collectionResourceRel = "todoitems")
 interface ToDoItemRepository extends PagingAndSortingRepository<ToDoItem,Integer> {
